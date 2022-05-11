a3bc13e8dfa0d57e9f624dead93e8c36a86644b0
leeminju531 <dlalswn531@naver.com>
2022-05-12 05:52:56 +0900

    Add solution page

--- problem_solving/src/App.vue
+++ problem_solving/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  
+  <Solving/>
 </template>
 
 <script>
-
+import Solving from './components/Solving.vue'
 export default {
   name: 'App',
   components: {
-    
+    Solving
   }
 }
 </script>
@@ -19,6 +19,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ problem_solving/src/components/Solving.vue
@@ -0,0 +1,16 @@
+<template>
+<Header/>
+    <!-- <h1>Hello COALER, Welcome To COALA</h1> -->
+</template>
+
+<script>
+import Header from './Header.vue'
+export default{
+    name:'Home',
+    components:{
+        Header
+    },
+
+
+}
+</script>
