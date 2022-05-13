c1d9a42a67f842acf219971c2841dd86ab8593df
leeminju531 <dlalswn531@naver.com>
2022-05-14 03:03:16 +0900

    Update vue3 to vue2

---
+++ problem_solving/src/components/TempHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div style="background-color: powderblue;text-align:center;">
+    <p>임시 헤더 </p>
+</div>
+</template>
+
+<script>
+export default{
+    name:'TempHeader'
+}
+</script>
+
+<style>
+
+</style>
